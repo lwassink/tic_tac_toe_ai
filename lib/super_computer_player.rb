a02f4dde0189c5ffc2c31bd98bd3a12cc35d52c9
App Academy Student <student@appacademy.io>
2016-09-16 15:10:22 -0700

    Build TicTacToeNode and SuperComputerPlayer

--- lib/super_computer_player.rb
+++ lib/super_computer_player.rb
@@ -2,6 +2,13 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    children = node.children
+    
+    winner = children.find { |child| child.winning_node?(mark) }
+    return winner.prev_move_pos unless winner.nil?
+
+    children.find { |child| !child.losing_node?(mark) }.prev_move_pos
   end
 end
 
--- lib/tic_tac_toe_node.rb
+++ lib/tic_tac_toe_node.rb
@@ -1,17 +1,68 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
-  def losing_node?(evaluator)
+  def losing_node?(mark)
+    return true if @board.winner == other_mark(mark)
+    return false if children.empty?
+
+    if @next_mover_mark == mark
+      children.all? { |child| child.losing_node?(mark)}
+    else
+      children.any? { |child| child.losing_node?(mark)}
+    end
   end
 
-  def winning_node?(evaluator)
+  def winning_node?(mark)
+    return true if @board.winner == mark
+    return false if children.empty?
+
+    if @next_mover_mark == mark
+      children.any? { |child| child.winning_node?(mark)}
+    else
+      children.all? { |child| child.winning_node?(mark)}
+    end
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    empty_positions.map do |pos|
+      board = Board.new(deep_dup(@board.rows))
+      board[pos] = @next_mover_mark
+      child = TicTacToeNode.new(board,other_mark(@next_mover_mark),pos)
+    end
+  end
+
+  def other_mark(mark)
+    if mark == :x
+      :o
+    else
+      :x
+    end
+  end
+
+
+  def empty_positions
+    all_positions.select {|position| @board.empty?(position)}
+  end
+
+  def all_positions
+    [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]]
+  end
+
+  def deep_dup(array)
+    array.map do |row|
+      row.dup
+      # return el unless el.is_a?(Array)
+      # deep_dup(el)
+    end
   end
 end
